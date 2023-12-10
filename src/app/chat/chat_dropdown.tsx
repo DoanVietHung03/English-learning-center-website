76456ce3f5fd339ea7d8a8f522f30a29a27e9dc2
hung <hungdoan29102003@gmail.com>
2023-12-10 21:08:22 +0700

    addNodeModules

--- src/app/chat/chat_dropdown.tsx
+++ src/app/chat/chat_dropdown.tsx
@@ -17,7 +17,7 @@ export default function Example() {
       </div>
 
       <Transition
-        as={Fragment}
+        as = {Fragment}
         enter="transition ease-out duration-100"
         enterFrom="transform opacity-0 scale-95"
         enterTo="transform opacity-100 scale-100"
