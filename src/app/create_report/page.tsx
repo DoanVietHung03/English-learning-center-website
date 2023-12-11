6f3d6dd7be045e850f5b97e32b3064e7df79087c
hung <hungdoan29102003@gmail.com>
2023-12-11 21:09:49 +0700

    change

--- src/app/create_report/page.tsx
+++ src/app/create_report/page.tsx
@@ -4,6 +4,7 @@ import SideBar from "@/components/layout/sideBar"
 import Header from "@/components/layout/header"
 import RPtypeDropdown from "./reportType_dropdown"
 import Iimage from "@/components/icons/icon_image"
+import IfileExport from "@/components/icons/file_export"
 
 export default function create_RP() {
     return (
@@ -17,7 +18,7 @@ export default function create_RP() {
                             Feedback and Bug report
                         </div>
 
-                        <div className="bg-white rounded">
+                        <div className="bg-white rounded pb-6">
                             <div className="flex items-center gap-24 pt-12">
                                 <div className=" ml-14 w-1/3">
                                     <p className="text-black text-base font-medium leading-tight tracking-tight">Title of report (*)</p>
@@ -35,20 +36,21 @@ export default function create_RP() {
                                 <textarea className="w-full h-[340px] border-2 rounded-md mt-4 pt-2 pl-2 focus:outline-none" id="myContent" placeholder="Type content of the report"></textarea>
                             </div>
 
-                            <button className="flex items-center ml-14 mt-2 gap-2 bg-gray-200 rounded-xl px-3 py-2">
+                            <button className="flex items-center ml-14 mt-2 gap-2 bg-gray-200 rounded-xl px-3 py-2 hover:bg-gray-300">
                                 <Iimage />
                                 <p className=" text-zinc-500 text-base leading-tight tracking-tight">Attached image</p>
                             </button>
 
                             <div className="rounded-md border border-zinc-400 ml-14 mr-12 mt-2 pl-2 py-1">
-                                <button className="bg-zinc-300 border border-black text-sm px-5">
+                                <button className="bg-zinc-300 border border-black text-sm px-5 hover:bg-zinc-400">
                                     Choose file
                                 </button>
                             </div>
 
-                            <div>
-                                <button>
-                                    
+                            <div className="ml-14 mt-6">
+                                <button className="flex items-center gap-2 bg-lime-300 rounded-lg px-4 py-1">
+                                    <p>Send</p>
+                                    <IfileExport />
                                 </button>
                             </div>
 
