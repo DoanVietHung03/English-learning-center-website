69420df3d8503346e2f61c11df7891ee38edc27e
hung <hungdoan29102003@gmail.com>
2023-12-12 08:59:08 +0700

    change

--- src/app/exercise_bank/page.tsx
+++ src/app/exercise_bank/page.tsx
@@ -5,6 +5,8 @@ import Header from "@/components/layout/header"
 import SubjectDropdown from "./subjectDropdown"
 import ModuleDropdown from "./moduleDropdown"
 import SkillDropdown from "./skillDropdown"
+import Link from "next/link"
+import Ieye from "@/components/icons/eye"
 
 export default function Profile() {
     return (
@@ -35,9 +37,24 @@ export default function Profile() {
                         </div>
                     </div>
 
-                    <div className="flex justify-between">
-                        <div className="w-1/3">
+                    <div className="flex justify-between mt-10">
+                        <div className="w-1/3 bg-white rounded-xl border border-zinc-300 px-2 pb-4">
+                            <p className="text-center text-black text-base font-semibold font-['Poppins'] mt-4">IELTS Academic Grammar 0 - 2.5 | Tân ngữ</p>
+                            <div className="flex items-center justify-between mt-4 border-b border-stone-200 pb-4">
+                                <p className="rounded-md border-2 border-stone-300 text-center text-stone-300 text-base font-semibold font-['Poppins'] px-1">IELTS</p>
+                                <p className="rounded-md border-2 border-stone-300 text-center text-stone-300 text-base font-semibold font-['Poppins'] px-1">Academic</p>
+                                <p className="rounded-md border-2 border-stone-300 text-center text-stone-300 text-base font-semibold font-['Poppins'] px-1">Grammar</p>
+                                <p className="rounded-md border-2 border-stone-300 text-center text-stone-300 text-base font-semibold font-['Poppins'] px-1">0 - 2.5</p>
+                            </div>
 
+                            <div className="mt-10 ml-4">
+                                <Link href={""}>
+                                    <button className="flex items-center gap-2 bg-stone-300 rounded-md px-3 py-1 hover:bg-stone-200">
+                                        <Ieye className="w-[1em] fill-blue-400"/>
+                                        <p className="text-sky-400 text-sm font-medium font-['Poppins']">View Exercise</p>
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
 
                         <div className="w-1/3">
