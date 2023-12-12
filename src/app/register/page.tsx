5f4479aa11f5985da7af6278cd9628f0f6de9c94
unknown <kduy2019@gmail.com>
2023-12-12 17:38:16 +0700

    them type

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,17 +2,31 @@
 import SideBar from "@/components/layout/sideBar"
 import Header from "@/components/layout/header"
 import { SyntheticEvent, useState } from "react"
+import Select from "react-select";
+
+const options = [
+    { value: "Student", label: "Student" },
+    { value: "Teacher", label: "Teacher" },
+];
+
 export default function Register() {
     const [phone, setPhone] = useState('');
     const [password, setPassword] = useState('');
+    const [type, setType] = useState('');
+
+    const handleChange = (ev) => {
+        setType(ev.value);
+        console.log(type);
+    };
     async function handleFormSubmit(ev: SyntheticEvent) {
         ev.preventDefault()
         await fetch('/api/register', {
             method: 'POST',
-            body: JSON.stringify({ phone, password }),
+            body: JSON.stringify({ phone, password, type }),
             headers: { 'Content-Type': 'application/json' },
         })
     }
+
     return (
         <>
             <Header />
@@ -28,6 +42,8 @@ export default function Register() {
                                 onChange={ev => setPhone(ev.target.value)} />
                             <input className="block w-full bg-gray-200 px-4 py-2 rounded-lg mb-4" type="password" placeholder="password" value={password}
                                 onChange={ev => setPassword(ev.target.value)} />
+                            <div>User Type</div>
+                            <Select options={options} onChange={handleChange} autoFocus={true} className="mb-3" />
                             <button className="w-full" type="submit" onClick={handleFormSubmit}>Register</button>
                         </form>
                     </div>
--- src/models/user.ts
+++ src/models/user.ts
@@ -3,6 +3,7 @@ import { Schema, model, models } from "mongoose";
 const userSchema = new Schema({
     phone: { type: String, required: true, unique: true },
     password: { type: String, required: true, },
+    type: { type: String, required: true },
 }, { timestamps: true });
 
 export const User = models?.User || model('User', userSchema)
