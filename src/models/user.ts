a7b9773455716d1a4a209cfd98cfc1c12810c42a
unknown <kduy2019@gmail.com>
2023-12-12 13:55:07 +0700

    validate register

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,16 +2,7 @@ import { Schema, model, models } from "mongoose";
 
 const userSchema = new Schema({
     phone: { type: String, required: true, unique: true },
-    password: {
-        type: String,
-        required: true,
-        validate: (pass: string | any[]) => {
-            if (!pass?.length || pass.length < 5) {
-                new Error('password must be at least 5 characters')
-                return false
-            }
-        },
-    },
+    password: { type: String, required: true, },
 }, { timestamps: true });
 
 export const User = models?.User || model('User', userSchema)
