8fe64a2e94b45895e2b1b5e90d5de65654075e18
hung <hungdoan29102003@gmail.com>
2023-12-12 09:56:38 +0700

    change

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,7 +1,7 @@
 import { User } from "@/models/user"
 import mongoose from "mongoose"
 
-export async function POST(req) {
+export async function POST(req: { json: () => any }) {
     const body = await req.json()
     mongoose.connect("mongodb+srv://learning-management:Abuo65lscK5pOUms@cluster0.nwhbe5i.mongodb.net/learning-management")
     const createdUser = await User.create(body)
