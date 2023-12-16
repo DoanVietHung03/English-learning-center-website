4c85928b4f4f2764eddb1251ed6d336e9131cd32
unknown <kduy2019@gmail.com>
2023-12-16 12:09:54 +0700

    admin user management

---
+++ src/app/user_management/page.tsx
@@ -0,0 +1,78 @@
+'use client'
+import SideBar from "@/components/layout/sideBar"
+import Header from "@/components/layout/header"
+import Link from "next/link"
+import Icalendar from "@/components/icons/icon_cal"
+import Ibook from "@/components/icons/icon_book"
+import { useEffect, useState } from "react"
+import Iteacher from "@/components/icons/icon_teacher"
+import Istudent from "@/components/icons/icon_student"
+export default function UserManagement() {
+    const [teachers, setTeachers] = useState([]);
+    const [students, setStudents] = useState([]);
+    useEffect(() => {
+        fetch('/api/user')
+            .then(res => res.json())
+            .then(data => {
+                setTeachers(data.teachers)
+                setStudents(data.students)
+            })
+    }, []);
+    const noStudent = students.length
+    const noTeacher = teachers.length
+    console.log(teachers)
+    return (
+        <>
+            <Header />
+            <div className="flex gap-4">
+                <SideBar />
+                <div className="w-3/4 mr-8">
+                    <div className="flex mt-2 pb-1 w-full border-b justify-between border-black">
+                        <div className="font-poppins font-bold text-5xl ">
+                            User Manangement
+                        </div>
+                        <Link href='/register' className="bg-lime-400 flex gap-3 rounded-xl items-center justify-center py-3 px-3">
+                            <div className="text-2xl font-bold">+</div>
+                            <div className="font-bold flex items-center">Add User</div>
+                        </Link>
+                    </div>
+                    <div className="bg-white flex mt-3 rounded-lg pt-6 pb-6">
+                        <div className="w-1/2 flex flex-col gap-3 border-r-2 border-black pr-7">
+                            <div className="flex items-center justify-center font-bold text-xl">Teacher</div>
+                            {
+                                teachers.map(teacher => (
+                                    // eslint-disable-next-line react/jsx-key
+                                    <div className="flex gap-3 p-3 items-center bg-gray-300 ml-5 rounded-xl">
+                                        <Iteacher className="flex w-8 fill-gray-500" />
+                                        <div className="flex gap-4">
+                                            <div className="text-gray-500">{teacher.phone}</div>
+                                            <div className="text-gray-500"> {teacher.name}</div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                        <div className="w-1/2 flex flex-col gap-3 pr-6">
+                            <div className="flex items-center justify-center font-bold text-xl">Student</div>
+                            {
+                                students.map(student => (
+                                    // eslint-disable-next-line react/jsx-key
+                                    <div className="flex gap-3 p-3 items-center bg-gray-300 ml-5 rounded-xl">
+                                        <Istudent className="flex w-8 fill-gray-500" />
+                                        <div className="flex gap-4">
+                                            <div className="text-gray-500">{student.phone}</div>
+                                            <div className="text-gray-500"> {student.name}</div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </>
+    )
+}
