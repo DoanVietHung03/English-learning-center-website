7270e030daeb829319f0e49a11fc850b2f3363cb
Tran <dbtran21@clc.fitus.edu.vn>
2023-12-18 15:19:22 +0700

    edit submission

--- src/app/api/grade/route.ts
+++ src/app/api/grade/route.ts
@@ -4,12 +4,10 @@ import mongoose from "mongoose"
 export async function POST(req: { json: () => any }) {
     const body = await req.json()
     mongoose.connect("mongodb+srv://learning-management:Abuo65lscK5pOUms@cluster0.nwhbe5i.mongodb.net/learning-management")
-    const createdSubmission = await Submission.create({
-        student_id: body.id_student,
-        assignment_id: body.id_assignment,
-        answer: body.answer,
-        grade: null,
-        attachedFile: body.file
-    })
-    return Response.json(createdSubmission);
+    //const IdAssignmentUpDate = new ObjectId('yourObjectId');
+    const updatedGrade = {
+        grade: body.grade
+    };
+    const submissionEdit = await Submission.updateOne({ _id: body.id }, { $set: updatedGrade });
+    return Response.json(submissionEdit);
 }
