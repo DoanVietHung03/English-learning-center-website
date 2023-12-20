149ab58457ba51ea10265920585ed73568eb5e7f
hung <hungdoan29102003@gmail.com>
2023-12-20 16:26:10 +0700

    update sidebar

--- src/components/layout/sideBar.tsx
+++ src/components/layout/sideBar.tsx
@@ -8,14 +8,25 @@ import dynamic from "next/dynamic"
 export default function SideBar() {
     const type = localStorage.getItem('userType')
     const [open, setOpen] = useState(true);
+
+    const IconBook1 = dynamic(() => import('../icons/icon_book1'));
+    const IconBank = dynamic(() => import('../icons/icon_bank'));
+    const IconFlag = dynamic(() => import('../icons/icon_flag'));
+    const IconChat = dynamic(() => import('../icons/icon_chat'));
+    const IconUser = dynamic(() => import('../icons/icon_user'));
+
     const Menus = [
-        { title: "Course List", src: dynamic(() => import("../icons/icon_book1")) },
-        { title: "Exercise Bank", src: dynamic(() => import("../icons/icon_bank")) },
-        { title: "Feedback and Bug Report", src: dynamic(() => import("../icons/icon_flag")) },
-        { title: "Chat", src: dynamic(() => import("../icons/icon_chat")) },
-        { title: "User Management", src: dynamic(() => import("../icons/icon_user")) }
-    ];
+        { title: 'Course List', src: <IconBook1 className="w-8 fill-white" /> },
+        { title: 'Exercise Bank', src: <IconBank className="w-8 fill-white" /> },
+        { title: 'Bug Report', src: <IconFlag className="w-8 fill-white" /> },
+        { title: 'Chat', src: <IconChat className="w-8 fill-white" /> },
+        { title: 'User Management', src: <IconUser className="w-8 fill-white" /> },
+      ];
+
     const paths = ["/courseList", "/exercise_bank", "/report_bug", "chat", "/user_management"]
+
+    const menuIconAdmin = Menus[4].src;
+    const menuIconNotAdmin = Menus[3].src;
 
     return (
         <div className="flex bg-[#68C6E3] rounded-tr-3xl rounded-br-3xl mt-16 h-fit pb-8">
@@ -36,11 +47,12 @@ export default function SideBar() {
                 </div>
                 <ul className="-pt-2">
                     {Menus.slice(0, 4).map((Menu, index) => (
+                        
                         <Link key={index} href={index < 3 ? paths[index] : (type === 'Admin' ? "/user_management" : "/chat")}>
                             <li
                                 className={`w-full mt-6 p-2 flex cursor-pointer rounded-lg text-white font-bold text-base items-center gap-x-4 hover:bg-sky-300 transition-colors
                                             ${index === 0 && "bg-transparent"}`}>
-                            <Menu.src className="w-8 fill-white"/>
+                                {index < 3 ? (Menu.src) : (type !== 'Admin' ? <IconChat className="w-8 fill-white"/> : <IconUser className="w-8 fill-white"/>)}
                                 <span className={`${!open && "hidden"} origin-left duration-200 ml-4`}>
                                     {index < 3 ? Menu.title : (type !== 'Admin' ? Menus[3].title : Menus[4].title)}
                                 </span>
