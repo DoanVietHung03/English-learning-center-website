5918b18e20140bae645e124ce3361420df891cab
Tran <dbtran21@clc.fitus.edu.vn>
2023-12-23 14:30:09 +0700

    update report

--- src/app/api/report_list/route.ts
+++ src/app/api/report_list/route.ts
@@ -14,6 +14,8 @@ export async function POST(req: { json: () => any }) {
     else{
         reports = await Report.find({userID: body.id})
     }
+    reports = reports
+      .filter(report => report !== null);
     return Response.json(reports);
   } catch (error) {
     return new Response(
