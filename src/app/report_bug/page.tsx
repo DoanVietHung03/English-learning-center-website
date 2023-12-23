832bc49f718eef0442bb3e2a7d21a1ec237d49a3
hung <hungdoan29102003@gmail.com>
2023-12-23 14:30:36 +0700

    RP BUG

--- src/app/report_bug/page.tsx
+++ src/app/report_bug/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link"
 import Select from "react-select";
 import * as React from 'react';
 import { ReactElement, useState, useEffect } from "react"
+import moment from 'moment';
 
 export default function RP() {
     const [status, setStatus] = useState('')
@@ -27,13 +28,7 @@ export default function RP() {
         })
             .then(response => response.json())
             .then(data => {
-                // Hiển thị danh sách khóa học trong giao diện
                 setReports(data)
-                console.log(data)
-                // if (courses == null)
-                //     setEmptyCourse(true)
-                // else
-                //     setEmptyCourse(false)
             })
             .catch(error => console.error('Error:', error));
     })
@@ -43,7 +38,7 @@ export default function RP() {
             <Header />
             <div className="flex">
                 <SideBar />
-                <div className="ml-14 w-2/3">
+                <div className="ml-14 w-3/4">
                     <div className="mb-4 mt-4 font-poppins font-bold text-5xl border-b border-black pb-2">
                         Feedback and Bug report
                     </div>
@@ -53,46 +48,87 @@ export default function RP() {
                             <Link href="/create_report">
                                 <button className="flex items-center gap-2 px-4 py-4 bg-lime-300 rounded-lg justify-center hover:bg-lime-400">
                                     <Ihand />
-                                    <p className="text-center text-black text-sm font-['Poppins'] leading-tight tracking-tight">Create report</p>
+                                    <div className="text-center text-black text-sm font-['Poppins'] leading-tight tracking-tight">Create report</div>
                                 </button>
                             </Link>
                         </div>
 
                         <div className="ml-10 mt-4 w-[141px]">
-                            <Select 
+                            <Select
                                 options={optionStatus}
                                 onChange={handleChangeStatus}
                                 placeholder="Status" />
                         </div>
 
                         <div className="mt-12 mx-10">
-                            <div className="flex items-center bg-zinc-300 justify-between text-center text-black text-base font-bold leading-tight tracking-tight px-4">
-                                <p>ID</p>
-                                <p>Title</p>
-                                <p>Type</p>
-                                <p>Status</p>
-                                <p>Date Created</p>
-                                <p>Date Completed</p>
-                            </div>
-                            <div className="flex items-center gap-16 text-center text-black text-xs leading-tight tracking-tight px-1 py-1 mt-1 border-b border-stone-300">
-                                <p>AOE-48</p>
-                                <p className="ml-3">Infor not display</p>
-                                <p className="bg-rose-200 text-red-600 font-semibold px-2 py-1">System issue</p>
-                                <p className="ml-2">Completed</p>
-                                <p className="ml-10">20:39 04/06/2023</p>
-                                <p className="ml-16">10:45 07/06/2023</p>
-                            </div>
-
-                            <div className="flex items-center gap-16 text-center text-black text-xs leading-tight tracking-tight px-1 py-1 mt-1 border-b border-stone-300">
-
-                            </div>
-
-                            <div className="flex items-center gap-16 text-center text-black text-xs leading-tight tracking-tight px-1 py-1 mt-1 border-b border-stone-300">
-
-                            </div>
+                            {reports.map((rep, index) => (
+                                <>
+                                    <div key={index} className="grid grid-cols-6 items-center">
+                                        <div>
+                                            <p className="bg-zinc-300 text-black text-base font-bold leading-tight tracking-tight">ID</p>
+                                            <div key={index}>
+                                                <div className="flex items-center justify-between text-center text-black text-xs leading-tight tracking-tight px-1 py-1 mt-1 border-b border-stone-300">
+                                                    <div>{rep.userID}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div >
+                                            <p className="bg-zinc-300 text-black text-base font-bold leading-tight tracking-tight">Title</p>
+                                            <div key={index}>
+                                                <div className="flex items-center justify-between text-center text-black text-xs leading-tight tracking-tight px-1 py-1 mt-1 border-b border-stone-300">
+                                                    <div>{rep.title}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div >
+                                            <p className="bg-zinc-300 text-black text-base font-bold leading-tight tracking-tight">Type</p>
+                                            <div key={index}>
+                                                <div className="flex items-center justify-between text-center text-black text-xs leading-tight tracking-tight px-1 py-1 mt-1 border-b border-stone-300">
+                                                    <div>{rep.type}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div >
+                                            <p className="bg-zinc-300 text-black text-base font-bold leading-tight tracking-tight">Date Created</p>
+                                            <div key={index}>
+                                                <div className="flex items-center justify-between text-center text-black text-xs leading-tight tracking-tight px-1 py-1 mt-1 border-b border-stone-300">
+                                                    <div>{moment.utc(rep.date_created).format('MM/DD/YYYY')}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div >
+                                            <p className="bg-zinc-300 text-black text-base font-bold leading-tight tracking-tight">Date Completed</p>
+                                            <div key={index}>
+                                                <div className="flex items-center justify-between text-center text-black text-xs leading-tight tracking-tight px-1 py-1 mt-1 border-b border-stone-300">
+                                                    <div>{(rep.date_completed === null ? <div>Not yet</div> : moment.utc(rep.date_completed).format('MM/DD/YYYY'))}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div >
+                                            <p className="bg-zinc-300 text-black text-base font-bold leading-tight tracking-tight">Status</p>
+                                            <div key={index}>
+                                                <div className="flex items-center justify-between text-center text-black text-xs leading-tight tracking-tight px-1 py-1 mt-1 border-b border-stone-300">
+                                                    <div>{rep.status}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    </>))}
                         </div>
-
+                        {/* {reports.map((rep, index) => (
+                                <div key={index}>
+                                    <div className="flex items-center justify-between text-center text-black text-xs leading-tight tracking-tight px-1 py-1 mt-1 border-b border-stone-300">
+                                        <div>{rep.userID}</div>
+                                        <div className="ml-3">{rep.title}</div>
+                                        <div className="bg-rose-200 text-red-600 font-semibold px-2 py-1">{rep.type}</div>
+                                        <div className="ml-2">{rep.status}</div>
+                                        <div className="ml-10">{moment.utc(rep.date_created).format('MM/DD/YYYY')}</div>
+                                        <div className="ml-16">{moment.utc(rep.date_completed).format('MM/DD/YYYY')}</div>
+                                    </div>
+                                </div>
+                            ))} */}
                     </div>
+
                 </div>
             </div>
         </>
