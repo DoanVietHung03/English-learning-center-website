0a4b3646cfb1c8713fb6e34883f73d241aa79182
Tran <dbtran21@clc.fitus.edu.vn>
2024-01-07 17:50:31 +0700

    chat avatar

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -26,10 +26,22 @@ export default function Chat() {
     const [message_sent, SetMessageSent] = useState([]);
     const [message_received, SetMessageReceived] = useState([]);
     const [image, setImage] = useState("")
+    var delete_message = ''
 
     const handleChangeReceiver = (ev) => {
         setReceiver(ev.value);
     };
+
+    async function handleDelete(ev: SyntheticEvent) {
+        ev.preventDefault()
+        console.log(delete_message)
+        const response = await fetch('/api/message', {
+            method: 'POST',
+            body: JSON.stringify({ id: delete_message, method: 'delete' }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+        window.location.reload(true);
+    }
 
     useEffect(() => {
         localStorage.setItem('sidebar', 3)
@@ -169,9 +181,9 @@ export default function Chat() {
 
                                                                 <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[500px] h-44 bg-gray-200 p-4 border-2 border-gray-500 rounded-lg">
                                                                     <div className="mt-4">
-                                                                        <p className="text-center text-lg font-semibold">Do you want to delete permanently ?</p>
+                                                                        <p className="text-center text-lg font-semibold">Do you want to delete permanently?</p>
                                                                         <div className="flex items-center justify-between mt-10 gap-2 text-lg font-medium">
-                                                                            <button className="w-1/2 border-2 border-black bg-lime-400 hover:bg-lime-500 rounded-md py-2" onClick={ev => { delete_course = course.course_id, handleDelete(ev) }}>
+                                                                            <button className="w-1/2 border-2 border-black bg-lime-400 hover:bg-lime-500 rounded-md py-2" onClick={ev => { delete_message = mes_send._id, handleDelete(ev) }}>
                                                                                 Yes
                                                                             </button>
 
@@ -278,7 +290,7 @@ export default function Chat() {
                                                                     <div className="mt-4">
                                                                         <p className="text-center text-lg font-semibold">Do you want to delete permanently ?</p>
                                                                         <div className="flex items-center justify-between mt-10 gap-2 text-lg font-medium">
-                                                                            <button className="w-1/2 border-2 border-black bg-lime-400 hover:bg-lime-500 rounded-md py-2" onClick={ev => { delete_course = course.course_id, handleDelete(ev) }}>
+                                                                            <button className="w-1/2 border-2 border-black bg-lime-400 hover:bg-lime-500 rounded-md py-2" onClick={ev => { delete_message = mes_receive._id, handleDelete(ev)}}>
                                                                                 Yes
                                                                             </button>
 
